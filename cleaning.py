61582928b62da77847ecc88b029ad22367345f86
BEARYLENOVO\Admin <barrylim1996@gmail.com>
2018-12-21 15:47:11 +0800

    Updated cleaning code

--- cleaning.py
+++ cleaning.py
@@ -7,36 +7,55 @@ pd.set_option('display.max_columns',50)
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
-## Drop non-useful columns
+columns = ['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
 
-train.drop(columns = ['PassengerId','Name','Ticket','Cabin',],inplace = True)
+#for i in columns:
+#    print(train[i].isnull().values.any())
 
+#for i in columns:
+#    if i == "Survived":
+#        continue
+#    else:
+#        print(test[i].isnull().values.any())
+
+
+##### "Sex" #####
 ## Replace values with 0 if female , 1 if male
 
 train['Sex'] = train['Sex'].map({'female': 0, 'male': 1})
 test['Sex'] = test['Sex'].map({'female': 0, 'male': 1})
 
+##### "Age" #####
 ## Fill missing values of Age with mean
 
-median= train['Age'].median()
-train['Age'].fillna(median,inplace=True)
+train_median= train['Age'].median()
+train['Age'].fillna(train_median,inplace=True)
 
+test_median= test['Age'].median()
+test['Age'].fillna(test_median,inplace=True)
 
+##### "Embarked" #####
 ## replace c = 1 , q = 2, s = 3
 
 train['Embarked'] = train['Embarked'].map({'C': 1, 'Q': 2, 'S': 3})
+test['Embarked'] = test['Embarked'].map({'C': 1, 'Q': 2, 'S': 3})
 
 ## replace 2 missing values of Embarked with mean
 
-mean = train['Embarked'].mean()
-train['Embarked'].fillna(mean,inplace=True)
+train_mean = train['Embarked'].mean()
+train['Embarked'].fillna(train_mean,inplace=True)
 
-df_X = train[train.columns[train.columns != 'Survived']].copy()
+test_mean = train['Embarked'].mean()
+train['Embarked'].fillna(test_mean,inplace=True)
 
-df_y = train['Survived'].copy()
 
-X_train, X_test, y_train, y_test = train_test_split(df_X, df_y, test_size=0.3, random_state=25)
+##### "Fare" #####
+test_median= test['Fare'].median()
+test['Fare'].fillna(test_median,inplace=True)
 
+
+train.to_csv('cleaned_train.csv',index=False)
+test.to_csv('cleaned_test.csv',index=False)
 
 
 
